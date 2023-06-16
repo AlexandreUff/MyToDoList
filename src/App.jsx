70f60041dcc567453d98cc519774cf92b931a5d8
AlexandreUff <alexandre_magno@id.uff.br>
2023-06-16 16:22:26 -0300

    feat: put RRD and create some routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import Header from './components/Header';
-import MainHome from './components/Main';
+import MainHome from './components/MainHome';
+import MainList from './components/MainList';
+import MainAbout from './components/MainAbout';
 import NavBar from './components/NavBar';
 import './index.css';
+
+import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
 
 function App() {
   return (
     <div className='main-container'>
       <Header />
       <NavBar />
-      <MainHome />
+      <Router>
+        <Routes>
+          <Route index element={<MainHome />} />
+          <Route path='/list' element={<MainList />} />
+          <Route path='/about' element={<MainAbout />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/MainAbout.jsx
@@ -0,0 +1,7 @@
+export default function MainHome(props){
+    return (
+        <main className="main-about">
+            How works TODO list...
+        </main>
+    )
+}
