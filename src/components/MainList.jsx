d15bfda7ab002066b905a32d81d6257bcf341ff2
AlexandreUff <alexandre_magno@id.uff.br>
2023-06-19 15:25:48 -0300

    feat: create email button and set his icon

--- src/components/MainList.jsx
+++ src/components/MainList.jsx
@@ -1,5 +1,5 @@
 import Button from "./Button";
-import { IcoClipBoardChecked } from "./Icons";
+import { IcoClipBoardChecked, IcoEmail } from "./Icons";
 import Item from "./Item";
 import NavBar from "./NavBar";
 
@@ -8,7 +8,10 @@ export default function MainHome(props){
         <main className="main-list">
             <NavBar />
             <section>
-                <Button icon={<IcoClipBoardChecked/>} text={"Criar um item"} action={()=>{console.log("itsWorks too")}} />
+                <div className="buttons-content">
+                    <Button icon={<IcoClipBoardChecked/>} text={"Criar um item"} action={()=>{console.log("itsWorks too")}} />
+                    <Button icon={<IcoEmail/>} text={"Enviar esta lista por e-mail"} action={()=>{console.log("itsWorks too")}} />
+                </div>
                 <div className="item-area">
                     <Item text="Este é um item very bom demais da conta partner"/>
                     <Item text="Este é um item very bom demais da conta partner"/>
