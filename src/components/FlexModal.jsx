686db13ed4c3052a817f3a1a34f94183e130aab5
AlexandreUff <alexandre_magno@id.uff.br>
2023-06-19 11:17:21 -0300

    chore: set styles for modal icon

--- src/components/FlexModal.jsx
+++ src/components/FlexModal.jsx
@@ -1,13 +1,19 @@
+import { IcoExclamation } from "./Icons";
+
 export default function FlexModal(props){
     return (
         <div className="ground-modal effect">
             <div className="modal">
+                <div className="ico-content">
+                    <IcoExclamation />
+                </div>
                 <h3>
                     {/* {props.message} */}
                     Teste testando tudo aqui porque everything induz à lokura
                 </h3>
                 <div>
-                    {props.children}
+                    {/* {props.children} */}
+                    <input type="text" />
                 </div>
             </div>
         </div>
