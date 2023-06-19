4168aa897f437051b8d2ca9ba06bf03401c8c779
AlexandreUff <alexandre_magno@id.uff.br>
2023-06-19 11:01:43 -0300

    chore: set styles for modal

--- src/components/FlexModal.jsx
+++ src/components/FlexModal.jsx
@@ -2,7 +2,10 @@ export default function FlexModal(props){
     return (
         <div className="ground-modal effect">
             <div className="modal">
-                <h3>{props.message}</h3>
+                <h3>
+                    {/* {props.message} */}
+                    Teste testando tudo aqui porque everything induz à lokura
+                </h3>
                 <div>
                     {props.children}
                 </div>
