05e89489e17b6922b963380365048ccc294d708b
AlexandreUff <alexandre_magno@id.uff.br>
2023-06-19 15:33:08 -0300

    chore: set done effect to Item Component

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,9 +1,15 @@
 import { /* IcoClipBoardEmpty, */ IcoEdit, IcoTrash } from "./Icons";
 
 export default function Item(props){
+
+    const checkItem = (e) => {
+        const parentElement = e.target.parentElement
+        parentElement.classList.toggle("done")
+    }
+
     return (
         <div className="item">
-            <input type="checkbox" />
+            <input type="checkbox" onChange={e => checkItem(e)} />
             <h3>
                 {props.text}
             </h3>
