9d44024b017f1c75d813bdfbd15e4d46aa3f1598
AlexandreUff <alexandre_magno@id.uff.br>
2023-06-20 11:55:30 -0300

    feat: set useParams in app and adjust create item

--- src/components/MainList.jsx
+++ src/components/MainList.jsx
@@ -1,15 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "./Button";
 import FlexModal from "./FlexModal";
 import { IcoCheckConfirm, IcoClipBoardChecked, IcoEmail, IcoX } from "./Icons";
 import Item from "./Item";
 import NavBar from "./NavBar";
+import StorageService from "../services/StorageService";
+import { useParams } from "react-router-dom";
 
 export default function MainHome(props){
 
-    const [modal, setModal] = useState(false)
+    const { listId } = useParams()
+    const listIdToNumber = +listId
 
-    const createListModal = () => {
+    const [showCreateModal, setShowCreateModal] = useState(false)
+    const [showEditModal, setShowEditModal] = useState(false)
+    const [showDeleteModal, setShowDeleteModal] = useState(false)
+    const [datas, setDatas] = useState([])
+
+    const storageDatas = StorageService.get("todo")
+
+    useEffect(() => {
+        console.log("itens", storageDatas)
+        if(storageDatas) setDatas([...storageDatas[listIdToNumber-1].itens])
+    },[])
+
+    const saveData = () => {
+        const inputValue = document.getElementById("create-list")
+        const dataToSave = {
+            id: datas.length+1,
+            name: inputValue.value,
+            order: datas.length+1,
+        }
+
+        const newStorageDatas = [...storageDatas]
+
+        newStorageDatas[listIdToNumber-1].itens.push(dataToSave)
+
+        StorageService.save("todo",
+            [
+            ...newStorageDatas,
+            ]
+        )
+        setDatas([...datas, dataToSave])
+        setShowCreateModal(!showCreateModal)
+    }
+
+    const createItemModal = () => {
         return (
             <FlexModal message={"Digite o nome de seu novo item:"}>
                 <input type="text" style={{
@@ -22,15 +58,17 @@ export default function MainHome(props){
                     color:"#E8F6EF",
                     fontSize:"2rem",
                     fontWeight: "600"
-                }} />
+                }}
+                    id="create-list"
+                 />
                 <div style={{
                     display:"flex",
                     justifyContent:"space-evenly",
                     alignItems:"center",
                     marginTop:"10px",
                 }}>
-                    <Button icon={<IcoCheckConfirm/>} text={"Criar"} action={()=>{console.log("itsWorks")}} />
-                    <Button icon={<IcoX/>} text={"Cancelar"} action={()=>{setModal(!modal)}} />
+                    <Button icon={<IcoCheckConfirm/>} text={"Criar"} action={()=>{saveData()}} />
+                    <Button icon={<IcoX/>} text={"Cancelar"} action={()=>{setShowCreateModal(!showCreateModal)}} />
                 </div>
             </FlexModal>
         )
@@ -41,16 +79,33 @@ export default function MainHome(props){
             <NavBar />
             <section>
                 <div className="buttons-content">
-                    <Button icon={<IcoClipBoardChecked/>} text={"Criar um item"} action={()=>{setModal(!modal)}} />
+                    <Button icon={<IcoClipBoardChecked/>} text={"Criar um item"} action={()=>{setShowCreateModal(!showCreateModal)}} />
                     <Button icon={<IcoEmail/>} text={"Enviar esta lista por e-mail"} action={()=>{console.log("itsWorks too")}} />
                 </div>
                 <div className="item-area">
+                    {/* <Item text="Este é um item very bom demais da conta partner" />
                     <Item text="Este é um item very bom demais da conta partner" />
-                    <Item text="Este é um item very bom demais da conta partner" />
-                    <Item text="Este é um item very bom demais da conta partner" />
+                    <Item text="Este é um item very bom demais da conta partner" /> */}
+                    {datas.map((item, i) => {
+                        return <Item
+                                    key={i}
+                                    text={item.name}
+                                    editMethod={() => {
+                                        /* setDataHandler({
+                                            id: item.id,
+                                            name:item.name
+                                        })
+                                        setShowEditModal(!showEditModal) */
+                                    }}
+                                    deleteMethod={() => {
+                                        /* setDataHandler(item.id)
+                                        setShowDeleteModal(!showDeleteModal) */
+                                    }}
+                                />
+                    })}
                 </div>
             </section>
-            {modal && createListModal()}
+            {showCreateModal && createItemModal()}
         </main>
     )
 }
