baccea7922150079ebac1e314aba1bd2ab96d2d2
AlexandreUff <alexandre_magno@id.uff.br>
2023-06-21 17:37:03 -0300

    style: identation for app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import Header from './components/Header';
-import MainHome from './components/MainHome';
-import MainList from './components/MainList';
-import MainAbout from './components/MainAbout';
-import './index.css';
+import Header from "./components/Header";
+import MainHome from "./components/MainHome";
+import MainList from "./components/MainList";
+import MainAbout from "./components/MainAbout";
+import "./index.css";
 
-import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className='main-container'>
+    <div className="main-container">
       <Header />
       <Router>
         <Routes>
           <Route index element={<MainHome />} />
-          <Route path='/list/:listId' element={<MainList />} />
-          <Route path='/about' element={<MainAbout />} />
-          <Route path='*' element={<MainHome />} />
+          <Route path="/list/:listId" element={<MainList />} />
+          <Route path="/about" element={<MainAbout />} />
+          <Route path="*" element={<MainHome />} />
         </Routes>
       </Router>
     </div>
